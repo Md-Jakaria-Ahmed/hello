e8d78fe84fda7c03f9b498ae10e84e701b61a6b2
Zakaria Ahmed <zakaria@Zakarias-MacBook-Air.local>
2023-03-26 10:57:48 +0600

    hello plugin

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package hello test
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Test Plugin
+Plugin URI: https://wedevs.com
+Description: this is just plugin
+Version: 1.0.0
+Author URI: https://www.md-zakaria-ahmed.blogspot
+*/
+
+// don't call the file directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
